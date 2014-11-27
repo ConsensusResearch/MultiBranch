a239905fbac53f147c2115b8b523dfa1f7d371ec
andruiman <andruiman@alscicon>
2014-11-27 20:13:27 +0300

    Statically linked executable

--- multibranch/compile.sh
+++ multibranch/compile.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-ghc -rtsopts   -XStandaloneDeriving -O2 constants.hs forging.hs postructures.hs multibranch.hs
+ghc -rtsopts -threaded -O2 -static -optl-pthread -optl-static -XStandaloneDeriving -O2 constants.hs forging.hs postructures.hs multibranch.hs
